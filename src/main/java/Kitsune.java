d5f1c682b1ba4b3c603809a7666d7be944181183
Phytal <38742977+Phytal@users.noreply.github.com>
2019-09-22 22:43:29 -0500

    migrate to gradle

--- src/main/java/Kitsune.java
+++ src/main/java/Kitsune.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Kitsune {
     public static void main(String args[]) throws Exception
     {
-        JDA jda = new JDABuilder("NjI1NDk4OTMyMjkzOTI2OTM0.XYgbSw.5hV7NvaLw49tbLY9715peIxWTRA").build();
+        JDA jda = new JDABuilder("NjI1NDk4OTMyMjkzOTI2OTM0.XYg7XA.OiNCnXEiaQtrjHAi89gSAjM1plg").build();
 
     }
 }
