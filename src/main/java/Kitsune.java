a8e4422019a0b26f47109d926ae3e94f4fbb4299
Phytal <38742977+Phytal@users.noreply.github.com>
2019-09-22 21:50:59 -0500

    initial commit

---
+++ src/main/java/Kitsune.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Kitsune {
+    public static void main(String args[]) throws Exception
+    {
+        JDA jda = new JDABuilder("NjI1NDk4OTMyMjkzOTI2OTM0.XYgbSw.5hV7NvaLw49tbLY9715peIxWTRA").build();
+
+    }
+}
